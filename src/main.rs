bba19a1498cf1d17e74a8056353d2f69f10246ad
zigen <hrlclb@gmail.com>
2021-03-17 00:45:18 +0900

    separate functions

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,20 @@ fn main() {
     println!("Your code goes here!");
 
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    execute_command(args)
+}
+
+fn execute_command(commands: Vec<String>) {
+    match &*commands[1] {
+        "init" => init(),
+        _ => println!("Initialized git directory"),
     }
+}
+
+fn init() {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+    println!("Initialized git directory")
 }
